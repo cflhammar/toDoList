af5001dc480d2fd2e4b6c7bcaafc5b04f8585070
Fredrik Hammar <fredrik.hammar@MacBook-Pro-som-tillhor-Fredrik.local>
2021-10-01 16:20:27 +0200

    todolists intreface

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import { useState } from "react";
 import "./App.css";
-import ToDoList from "./components/ToDoList";
+import ToDoList, { IToDoItem } from "./components/ToDoList";
+
+interface IToDoList {
+	id: number;
+	title: string;
+	list: Array<IToDoItem>;
+}
 
 const App: React.FunctionComponent = () => {
-	return (
-		<Box>
-			<ToDoList />
-		</Box>
-	);
+	const [toDoLists, setToDoLists] = useState<IToDoList[]>([]);
+
+	const handleAddToDoList = (title: string) => {
+		setToDoLists((currentToDoLists) => [...currentToDoLists, {
+			id: 1,
+			title: title,
+			list: newList;
+		}])
+	};
+
+	const renderToDoLists = () => {
+		if (toDoLists.length > 0) {
+			return toDoLists.map((toDoList) => {
+				<Box>
+					<ToDoList />
+				</Box>;
+			});
+		}
+	};
+
+	return;
+	<Button
+		onClick={() => {
+			setToDoLists;
+		}}
+	>
+		{" "}
+		Add new list
+	</Button>;
+	renderToDoLists();
 };
 
 export default App;
